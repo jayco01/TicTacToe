e2a8ce1b698879622552c59631c2b517b9021f39
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-20 16:16:32 -0600

    Refactor: used a .map to create the boxes

--- src/App.jsx
+++ src/App.jsx
@@ -66,26 +66,24 @@ function Board({ xIsNext, squares, onPlay }) {
   } else {
     status = "Next player: " + (xIsNext ? "X": "O");
   }
+  
+const boxes = Array.from({length:3}, () => Array(3).fill(null));
 
   return (
-    <>
-    <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-      </div>
-    </>
+    <div>
+      {boxes.map((row, rowIndex) => (
+
+        <div className="board-row" key={rowIndex}>
+          {row.map((cell, colIndex) => (
+
+            <div className="square" key={`${rowIndex}-${colIndex}`}>
+              <Square value={squares[(rowIndex * 3) + colIndex]} onSquareClick={() => handleClick((rowIndex * 3) + colIndex)}/>
+            </div>
+            
+          ))}
+        </div>
+      ))}
+    </div>
   );
 }
 
