1931d2af278ad4e3d5a186c545e6ce31a7f6aedf
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-08-21 17:45:33 -0600

    Feat: highlight winning line

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function Game() {
   const [currentMove, setCurrentMove] = useState(0);
   const xIsNext = currentMove % 2 === 0;
   const currentSquares= history[currentMove];
-
+  
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
@@ -42,7 +42,7 @@ export default function Game() {
   );
 }
 
-function Board({ xIsNext, squares, onPlay }) {
+function Board({ xIsNext, squares, onPlay}) {
   function handleClick(i) {
     if(squares[i] || calculateWinnner(squares)){
       return;
@@ -57,8 +57,12 @@ function Board({ xIsNext, squares, onPlay }) {
     }
     onPlay(nextSquares);
   }
+  
+  const result = calculateWinnner(squares);
+  const winner = (result === null)? null : result.winner;
+  const winningBoxes = (result === null)? Array(3).fill(-1) : result.boxes;
 
-  const winner = calculateWinnner(squares);
+  const boxes = Array.from({length:3}, () => Array(3).fill(null));
 
   let status;
   if (winner) {
@@ -66,32 +70,23 @@ function Board({ xIsNext, squares, onPlay }) {
   } else {
     status = "Next player: " + (xIsNext ? "X": "O");
   }
-  
-const boxes = Array.from({length:3}, () => Array(3).fill(null));
 
   return (
     <div>
       {boxes.map((row, rowIndex) => (
 
         <div className="board-row" key={rowIndex}>
+          
           {row.map((cell, colIndex) => (
 
             <div className="square" key={`${rowIndex}-${colIndex}`}>
-              <Square value={squares[(rowIndex * 3) + colIndex]} onSquareClick={() => handleClick((rowIndex * 3) + colIndex)}/>
+              <Square value={squares[(rowIndex * 3) + colIndex]} onSquareClick={() => handleClick((rowIndex * 3) + colIndex)} clazz="square" wBoxes={winningBoxes} boxNum={(rowIndex * 3) + colIndex}/>
             </div>
-            
+          
           ))}
         </div>
       ))}
     </div>
-  );
-}
-
-function Square({value, onSquareClick}) {
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button> 
   );
 }
 
@@ -110,8 +105,22 @@ function calculateWinnner(squares) {
   for(let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+
+      return { winner:squares[a], boxes:[a,b,c] };
     }
   }
   return null;
 }
+
+function Square({value, onSquareClick, clazz, wBoxes, boxNum}) {
+  if (wBoxes[0] == boxNum || wBoxes[1] == boxNum || wBoxes[2] == boxNum) {
+     clazz = "highlight"
+  }
+ 
+  return (
+    <button className={clazz} onClick={onSquareClick}>
+      {value}
+    </button> 
+  );
+}
+
